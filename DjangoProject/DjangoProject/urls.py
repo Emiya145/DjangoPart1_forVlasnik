aadc8027e22d1d3424c4c9436bbc8f1686a4ad1d
Emiya145 <98179459+Emiya145@users.noreply.github.com>
2025-09-21 23:03:41 -0400

    Add files via upload

---
+++ DjangoProject/DjangoProject/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for DjangoProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+from rest_framework.routers import DefaultRouter
+from movies.views import MovieViewSet
+
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),   # 👈 This is the key line
+    path('api/', include(router.urls)),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+settings.MEDIA_ROOT}), #serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+settings.STATIC_ROOT}), #serve static files when deployed
+
+]
