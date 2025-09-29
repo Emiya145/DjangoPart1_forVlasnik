930401f6df41bcf96f5e67913d4b87ad54eb4de2
Emiya145 <98179459+Emiya145@users.noreply.github.com>
2025-09-29 00:15:03 -0400

    Update asgi.py

--- DjangoProject/DjangoProject/asgi.py
+++ DjangoProject/DjangoProject/asgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.DjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
 
 application = get_asgi_application()
-
